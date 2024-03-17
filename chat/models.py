e7ad096cb7adc2907721ba14eb809466cabe1472
taylonchan <qwertty3566@gmail.com>
2024-03-17 18:32:19 +0800

    🗃️ | Create charactor table

--- chat/models.py
+++ chat/models.py
@@ -13,6 +13,15 @@ class Story(models.Model):
     def __str__(self):
         return str(self.id)
     
+class Charactor(models.Model):
+    id = models.BigAutoField(primary_key=True, editable=False)
+    name = models.TextField(max_length=100)
+    personality = models.TextField(max_length=1000, blank=True)
+    story_id = models.ForeignKey('Story', default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
+    
 class ChatItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     chat_history = models.JSONField(null=True, blank=True)
